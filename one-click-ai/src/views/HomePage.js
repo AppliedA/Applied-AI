00dc4abdd059ecf9532aad5ef77167bdfa79ae79
Vivek Patel <vivek.p9737@gmail.com>
2021-04-06 19:43:19 +0530

    hello world

---
+++ one-click-ai/src/views/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+export class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+            </div>
+        )
+    }
+}
+
+export default HomePage
